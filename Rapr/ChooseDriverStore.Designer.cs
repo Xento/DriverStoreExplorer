c437107f584c65ec9f87779fe8fdc90dd49fed1d
Markus M. <1299397+Xento@users.noreply.github.com>
2026-09-23 09:08:31 +0200

    Add remote computer controls

--- Rapr/ChooseDriverStore.Designer.cs
+++ Rapr/ChooseDriverStore.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class ChooseDriverStore
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,16 +15,14 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.radioButtonDriverStoreOnline = new System.Windows.Forms.RadioButton();
             this.radioButtonDriverStoreOffline = new System.Windows.Forms.RadioButton();
             this.textBoxOfflineStoreLocation = new System.Windows.Forms.TextBox();
             this.buttonBrowseLocation = new System.Windows.Forms.Button();
+            this.radioButtonDriverStoreRemote = new System.Windows.Forms.RadioButton();
+            this.textBoxRemoteComputer = new System.Windows.Forms.TextBox();
             this.buttonOK = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -75,31 +66,53 @@
             this.buttonBrowseLocation.UseVisualStyleBackColor = true;
             this.buttonBrowseLocation.Click += new System.EventHandler(this.ButtonBrowseLocation_Click);
             // 
+            // radioButtonDriverStoreRemote
+            // 
+            this.radioButtonDriverStoreRemote.AutoSize = true;
+            this.radioButtonDriverStoreRemote.Location = new System.Drawing.Point(12, 91);
+            this.radioButtonDriverStoreRemote.Name = "radioButtonDriverStoreRemote";
+            this.radioButtonDriverStoreRemote.Size = new System.Drawing.Size(103, 17);
+            this.radioButtonDriverStoreRemote.TabIndex = 4;
+            this.radioButtonDriverStoreRemote.TabStop = true;
+            this.radioButtonDriverStoreRemote.Text = global::Rapr.Lang.Language.DriverStore_RemoteMachine;
+            this.radioButtonDriverStoreRemote.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRemoteComputer
+            // 
+            this.textBoxRemoteComputer.Location = new System.Drawing.Point(13, 115);
+            this.textBoxRemoteComputer.Name = "textBoxRemoteComputer";
+            this.textBoxRemoteComputer.Size = new System.Drawing.Size(393, 20);
+            this.textBoxRemoteComputer.TabIndex = 5;
+            // 
             // buttonOK
             // 
             this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOK.Location = new System.Drawing.Point(116, 94);
+            this.buttonOK.Location = new System.Drawing.Point(116, 150);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
-            this.buttonOK.TabIndex = 4;
+            this.buttonOK.TabIndex = 6;
             this.buttonOK.Text = global::Rapr.Lang.Language.Button_OK;
             this.buttonOK.UseVisualStyleBackColor = true;
             // 
             // buttonCancel
             // 
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(235, 94);
+            this.buttonCancel.Location = new System.Drawing.Point(235, 150);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
-            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.TabIndex = 7;
             this.buttonCancel.Text = global::Rapr.Lang.Language.Button_Cancel;
             this.buttonCancel.UseVisualStyleBackColor = true;
             // 
-            // ChooseStoreLocation
+            // ChooseDriverStore
             // 
+            this.AcceptButton = this.buttonOK;
+            this.CancelButton = this.buttonCancel;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 129);
+            this.ClientSize = new System.Drawing.Size(418, 185);
+            this.Controls.Add(this.textBoxRemoteComputer);
+            this.Controls.Add(this.radioButtonDriverStoreRemote);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.buttonBrowseLocation);
@@ -109,14 +122,13 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "ChooseStoreLocation";
+            this.Name = "ChooseDriverStore";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = global::Rapr.Lang.Language.ChooseDriverStore_Title;
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
@@ -125,6 +137,8 @@
         private System.Windows.Forms.RadioButton radioButtonDriverStoreOffline;
         private System.Windows.Forms.TextBox textBoxOfflineStoreLocation;
         private System.Windows.Forms.Button buttonBrowseLocation;
+        private System.Windows.Forms.RadioButton radioButtonDriverStoreRemote;
+        private System.Windows.Forms.TextBox textBoxRemoteComputer;
         private System.Windows.Forms.Button buttonOK;
         private System.Windows.Forms.Button buttonCancel;
     }
